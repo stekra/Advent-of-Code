7287848b5f7cf2d274091914942c4303ff6596a3
stekra <stekra@me.com>
2022-02-19 19:19:14 +0100

    Update day 8

--- Advent of Code 2021.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
+++ Advent of Code 2021.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
@@ -8,6 +8,8 @@ import Foundation
 //6: 0,6,9
 //7: 8
 
+let startingTime = Date()
+
 let entries = input()
     .split(separator: "\n")
     .map {
@@ -15,57 +17,48 @@ let entries = input()
             .flatMap { $0.split(separator: " ") }
     }
 
-extension String {
-    mutating func add(_ chars: String) {
-        for c in chars {
-            if !self.contains(c) { self += "\(c)" }
-        }
-    }
-}
-
-extension StringProtocol {
-    func commonCharacters<S: StringProtocol>(with other: S) -> Int {
-        let intersection = Set(self).intersection(other)
-        return intersection.count
-    }
+enum Intermediate {
+    case raw(Substring)
+    case resolved(Int)
 }
 
 var outputValueSum = 0
 
 for line in entries {
-    var uniqueSegments = ""
-    var oneSegments = ""
+    var uniqueSegments: Set<Character> = []
+    var oneSegments: Set<Character> = []
     
     //uniques
-    let uniquesSolved = line.map { (pattern) -> Any in
+    let uniquesSolved = line.map { (pattern) -> Intermediate in
         switch pattern.count {
         case 2:
-            uniqueSegments.add(String(pattern))
-            oneSegments.add(String(pattern))
-            return 1
+            uniqueSegments.formUnion(pattern)
+            oneSegments.formUnion(pattern)
+            return .resolved(1)
         case 3:
-            uniqueSegments.add(String(pattern))
-            return 7
+            uniqueSegments.formUnion(pattern)
+            return .resolved(7)
         case 4:
-            uniqueSegments.add(String(pattern))
-            return 4
+            uniqueSegments.formUnion(pattern)
+            return .resolved(4)
         case 7:
-            return 8
+            return .resolved(8)
         default:
-            return pattern
+            return .raw(pattern)
         }
     }
     
     //disambiguate
-    let decodedLine = uniquesSolved.map { (pattern) -> Int in
-        if let string = pattern as? Substring {
-            let segCount = string.count
+    let decodedLine = uniquesSolved.map { (intermediate) -> Int in
+        switch intermediate {
+        case .raw(let pattern):
+            let segCount = pattern.count
             
             //0, 6, 9
             if segCount == 6 {
-                if string.commonCharacters(with: uniqueSegments) == 5 {
+                if uniqueSegments.intersection(pattern).count == 5 {
                     return 9
-                } else if string.commonCharacters(with: oneSegments) == 1 {
+                } else if oneSegments.intersection(pattern).count == 1 {
                     return 6
                 } else {
                     return 0
@@ -73,17 +66,16 @@ for line in entries {
                 
             //2, 3, 5
             } else /* segCount == 5 */ {
-                if string.commonCharacters(with: uniqueSegments) == 3 {
+                if uniqueSegments.intersection(pattern).count == 3 {
                     return 2
-                } else if string.commonCharacters(with: oneSegments) == 2 {
+                } else if oneSegments.intersection(pattern).count == 2 {
                     return 3
                 } else {
                     return 5
                 }
-                
             }
-        } else {
-            return pattern as! Int
+        case .resolved(let value):
+            return value
         }
     }
         
@@ -93,3 +85,5 @@ for line in entries {
 }
 
 print(outputValueSum)
+
+print("\(-startingTime.timeIntervalSinceNow) seconds elapsed")
